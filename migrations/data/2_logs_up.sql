c444678b2bbe8f39369c1496980cd203889c3389
Anton Van Eechaute <anton@wantedly.com>
2017-05-23 23:25:07 +0900

    Add logs migrations

---
+++ migrations/data/2_logs_up.sql
@@ -0,0 +1,16 @@
+CREATE SEQUENCE logs_seq;
+
+CREATE TYPE activity AS ENUM ('FLASHCARDS','TEXTBOOK','READING','LISTENING','TRANSLATION','GRAMMR','OTHER');
+CREATE TYPE language AS ENUM ('JA','KR','ZH','DE');
+
+CREATE TABLE logs (
+  id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('logs_seq'),
+  language language NOT NULL,
+  date date NOT NULL,
+  duration bigint check (duration > 0) NOT NULL,
+  activity activity NOT NULL,
+  notes jsonb,
+  PRIMARY KEY (id)
+);
+
+ALTER SEQUENCE logs_seq RESTART WITH 1;
