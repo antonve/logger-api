1629ee576dcb7a96ffc43b76f0de8bd5c7a11db9
Anton Van Eechaute <anton@wantedly.com>
2017-07-09 01:27:24 +0900

    Refactor serve helpers

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -13,63 +14,23 @@ import (
 	"github.com/labstack/echo"
 )
 
-// Return201 helper
-func Return201(context echo.Context) error {
-	return context.JSONBlob(http.StatusCreated, []byte(`{"success": true}`))
+// Serve a successful request
+func Serve(context echo.Context, statusCode int) error {
+	return context.JSONBlob(statusCode, []byte(`{"success": true}`))
 }
 
-// Return200 helper
-func Return200(context echo.Context) error {
-	return context.JSONBlob(http.StatusOK, []byte(`{"success": true}`))
-}
-
-// Return400 helper
-func Return400(context echo.Context, err error) error {
+// Serve a request with errors
+func ServeWithError(context echo.Context, statusCode int, err error) error {
 	handleError(err)
-	return Serve400(context)
-}
-
-// Serve400 helper
-func Serve400(context echo.Context) error {
-	return context.JSONBlob(http.StatusBadRequest, []byte(`{"success": false, "errorCode": 400, "errorMessage": "400 bad request"}`))
-}
-
-// Return403 helper
-func Return403(context echo.Context, err error) error {
-	handleError(err)
-	return Serve403(context)
-}
-
-// Serve403 helper
-func Serve403(context echo.Context) error {
-	return context.JSONBlob(http.StatusForbidden, []byte(`{"success": false, "errorCode": 403, "errorMessage": "400 forbidden"}`))
-}
-
-// Return404 helper
-func Return404(context echo.Context, err error) error {
-	handleError(err)
-	return Serve404(context)
-}
-
-// Serve404 helper
-func Serve404(context echo.Context) error {
-	return context.JSONBlob(http.StatusNotFound, []byte(`{"success": false, "errorCode": 404, "errorMessage": "404 page not found"}`))
-}
-
-// Serve405 helper
-func Serve405(context echo.Context) error {
-	return context.JSONBlob(http.StatusMethodNotAllowed, []byte(`{"success": false, "errorCode": 405, "errorMessage": "405 method not allowed"}`))
-}
-
-// Return500 helper
-func Return500(context echo.Context, err error) error {
-	handleError(err)
-	return Serve500(context)
-}
-
-// Serve500 helper
-func Serve500(context echo.Context) error {
-	return context.JSONBlob(http.StatusInternalServerError, []byte(`{"success": false, "errorCode": 500, "errorMessage": "500 internal server error"}`))
+	body := []byte(fmt.Sprintf(`
+		{
+			"success": false,
+			"errorCode": %d,
+			"errorMessage": "%s"
+		}`,
+		statusCode,
+		http.StatusText(statusCode)))
+	return context.JSONBlob(statusCode, body)
 }
 
 // getUser helper
