ea54bc3e50456483e1d916032a0be986495287f9
Anton Van Eechaute <anton@wantedly.com>
2017-10-09 14:35:06 +0900

    Change type of refresh_token

--- migrations/data/5_refresh_tokens_up.sql
+++ migrations/data/5_refresh_tokens_up.sql
@@ -4,7 +4,7 @@ CREATE TABLE refresh_tokens (
   id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('refresh_tokens_seq'),
   user_id bigint NOT NULL REFERENCES users (id),
   device_id varchar(32) NOT NULL,
-  refresh_token varchar(255) NOT NULL,
+  refresh_token bytea NOT NULL,
   created_at timestamp NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
   updated_at timestamp NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
   invalidated_at timestamp DEFAULT NULL,
