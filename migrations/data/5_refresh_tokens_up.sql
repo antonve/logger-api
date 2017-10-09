3df0ba9211f3b3e0d2fce26b70d5893f463b667f
Anton Van Eechaute <anton@wantedly.com>
2017-10-09 12:24:09 +0900

    Add migrations for refresh tokens table

---
+++ migrations/data/5_refresh_tokens_up.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE refresh_tokens_seq;
+
+CREATE TABLE refresh_tokens (
+  id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('refresh_tokens_seq'),
+  user_id bigint NOT NULL REFERENCES users (id),
+  device_id varchar(32) NOT NULL,
+  refresh_token varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+  updated_at timestamp NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+  invalidated_at timestamp DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+ALTER SEQUENCE refresh_tokens_seq RESTART WITH 1;
